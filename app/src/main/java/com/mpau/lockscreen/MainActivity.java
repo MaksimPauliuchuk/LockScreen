090176be6f5ebe7e3d9bca94414f40a037ae1930
mpau <bortwo@gmail.com>
2016-07-27 23:05:47 +0300

    Обработка событий swipe

--- app/src/main/java/com/mpau/lockscreen/MainActivity.java
+++ app/src/main/java/com/mpau/lockscreen/MainActivity.java
@@ -2,12 +2,20 @@ package com.mpau.lockscreen;
 
 import android.content.Intent;
 import android.os.Build;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    private float offSet = 0.0f;
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         startService(new Intent(this,MyService.class));
 
         setContentView(R.layout.activity_main);
+
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = new GestureDetectorCompat(this, this);
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this);
     }
 
     @Override
@@ -24,6 +40,74 @@ public class MainActivity extends AppCompatActivity {
         return; //Do nothing!
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDown: " + event.toString());
+        offSet = 0.0f;
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        if (offSet < 0.0f) {
+            Log.d(DEBUG_TAG, "motion: right");
+            unlockScreen();
+        } else {
+            Log.d(DEBUG_TAG, "motion: left");
+        }
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + e1.toString() + e2.toString());
+        offSet = e1.getX() - e2.getX();
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
+    }
     /**
      * A simple method that sets the screen to fullscreen.  It removes the Notifications bar,
      *   the Actionbar and the virtual keys (if they are on the phone)
@@ -40,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void unlockScreen(View view) {
+    public void unlockScreen() {
         android.os.Process.killProcess(android.os.Process.myPid());
     }
 }
